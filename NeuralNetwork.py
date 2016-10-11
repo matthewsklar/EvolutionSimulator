8b87424e5105bdd31f821636a1258bca2cd6908c
matthewsklar227@gmail.com <matthewsklar227@gmail.com>
2016-10-11 15:54:51 -0400

    Improve Documentation

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -3,88 +3,136 @@ import random
 import math
 
 
-# The neurons making up neuron layers
 class Neuron(object):
+    """
+    Individual neurons
+
+    Attributes:
+        inputs: An integer count of the inputs of the neuron
+        weights: A list of the weights of the neuron
+        bias: A float for the bias of the neuron
+        delta: A float for the delta (error) of the neuron
+        output: A float of the output of the neuron
+    """
+
     def __init__(self, inputs):
-        # Number if inputs in the neuron
+        """
+        Initializes the Neuron
+
+        Args:
+            inputs: An integer count of the inputs of the neuron
+        """
         self.inputs = inputs
-
-        # Weights in the neuron
         self.weights = []
-
-        # Bias of the neuron
         self.bias = random.random() * 2 - 1
-
-        # The error of the neuron
         self.delta = 0
-
-        # The output of the neuron
         self.output = 0
 
-        # Create the weights
         self.create_weights()
 
     def create_weights(self):
         """
-        Add a random number between -1 and 1 for each weight
+        Creates the weights by adding a random number between -1 and 1 for each weight
+
+        Raises:
+            TypeError: self.inputs cannot be interpreted as an integer
         """
         for x in range(0, self.inputs):
             self.weights.append(random.random() * 2 - 1)
 
 
 class NeuronLayer(object):
-    def __init__(self, num_neurons, inputs_per_neuron):
-        # Number of neurons in the layer
-        self.num_neurons = num_neurons
+    """
+    Layers of neurons: can be input, hidden, or output layer
 
-        # Neurons in the layer
+    Attributes:
+        num_neurons: An integer count of the neurons in the layer
+        neurons: A list of the Neurons in the layer
+    """
+
+    def __init__(self, num_neurons, inputs_per_neuron):
+        """
+        Initializes the Neuron
+
+        Args:
+            num_neurons: An integer count of the neurons in the layer
+            inputs_per_neuron: An integer count of the amount of inputs of each neuron in the layer
+        """
+        self.num_neurons = num_neurons
         self.neurons = []
 
-        # Create the neurons
         self.create_neurons(inputs_per_neuron)
 
     def create_neurons(self, inputs):
         """
-        Add the neurons to the layer
+        Creates the neurons and adds them to the list neurons
+
+        Args:
+            inputs: An integer count of the amount of inputs of each neuron in the layer
+
+        Raises:
+            TypeError: self.num_neurons cannot be interpreted as an integer
         """
         for i in range(0, self.num_neurons):
             self.neurons.append(Neuron(inputs))
 
 
 class NeuralNetwork(object):
+    """
+    Creates a neural network and runs it
+
+    Attributes:
+        num_inputs: An integer count of the input neurons
+        num_outputs: An integer count of the output neurons
+        num_hidden_layers: An integer count of the hidden layers
+        num_neurons_per_hidden_layer: An integer count of the neurons in a hidden layer
+        layers: A list of each NeuronLayer
+    """
+
     def __init__(self, num_inputs, num_outputs, num_hidden_layers, num_neurons_per_hidden_layer):
-        # Number of input neurons
+        """
+        Initializes the NeuralNetwork
+
+        Args:
+            num_inputs: An integer count of the input neurons
+            num_outputs: An integer count of the output neurons
+            num_hidden_layers: An integer count of the hidden layers
+            num_neurons_per_hidden_layer: An integer count of the neurons in a hidden layer
+        """
         self.num_inputs = num_inputs
-
-        # Number of output neurons
         self.num_outputs = num_outputs
-
-        # Number of hidden layers
         self.num_hidden_layers = num_hidden_layers
-
-        # Number of neurons in a hidden layer
         self.num_neurons_per_hidden_layer = num_neurons_per_hidden_layer
-
-        # Layers
         self.layers = []
 
     def create_network(self):
         """
-        Create the neural network
+        Creates the neural network
+
+        Raises:
+            TypeError: self.num_inputs, self.num_hidden_layers, self.num_neurons_per_hidden_layer, self.num_outputs
+            cannot all be interpreted as an integer
         """
-        # Create the input layer
-        self.layers.append(NeuronLayer(self.num_inputs, 1))
+        self.layers.append(NeuronLayer(self.num_inputs, 1))  # Create the input layer
 
-        # Create the hidden layers
         for i in range(0, self.num_hidden_layers):
-            self.layers.append(NeuronLayer(self.num_neurons_per_hidden_layer, self.layers[i - 1].num_neurons - 1))
+            self.layers.append(NeuronLayer(self.num_neurons_per_hidden_layer,
+                                           self.layers[i - 1].num_neurons - 1))  # Create the hidden layers
 
-        # Create the output layer
-        self.layers.append(NeuronLayer(self.num_outputs, self.num_neurons_per_hidden_layer))
+        self.layers.append(
+            NeuronLayer(self.num_outputs, self.num_neurons_per_hidden_layer))  # Create the output layer
 
     def calculate_network(self, inputs):
         """
         Calculate the output layer
+
+        Args:
+            inputs: A list of Neurons in the input layer
+
+        Returns:
+            A list of outputs of neurons from the output layer. For example
+
+            [.1, .5, .3]
         """
         # Stores the resultant outputs from each layer
         outputs = []
@@ -122,7 +170,7 @@ class NeuralNetwork(object):
                 net += j.bias
 
                 # Filter the output of the neuron then add it to the outputs
-                j.output = Sigmoid(net)
+                j.output = sigmoid(net)
 
                 outputs.append(j.output)
 
@@ -131,7 +179,7 @@ class NeuralNetwork(object):
         return outputs
 
 
-def Sigmoid(t):
+def sigmoid(t):
     """
     The sigmoid transfer function
               1
